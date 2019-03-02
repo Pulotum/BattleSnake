93898b613d990b340bf4e27714093ec5bdab24fc
Josh <j.larminay@gmail.com>
2019-03-02 10:18:06 -0800

    Minor

--- app/closestFood.py
+++ app/closestFood.py
@@ -1,11 +1,3 @@
-'''
-
-head    - array[ x,y ] cords of our snake head
-food    - array[ array[x,y] ] of all food items
-
-return  - array[ x,y ] of closest food item
-
-'''
 from operator import itemgetter
 
 
